17c7efb2c5c7180512cc77a1017c394241846141
ERoady87 <ericroady87@gmail.com>
2026-01-13 15:22:56 -0500

    AnimeObserverCache-WIP

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const { type } = require('express/lib/response');
+const mongoose = require('mongoose');
+
+const applicationSchema = mongoose.Schema({
+  company: { type: String, required: true },
+  title: { type: String, required: true },
+  notes: { type: String },
+  postingLink: { type: String },
+  status: { type: String, enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'], required: true }
+});
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  applications: [applicationSchema]
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
